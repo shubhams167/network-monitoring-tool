b7e329c02e723468f65a91e0060d0dc4fb34825e
Shubham Singh <shubhams.167@gmail.com>
2019-07-14 12:46:33 +0530

    All done

--- result.php
+++ result.php
@@ -24,11 +24,14 @@
             echo "<h2>" . strtoupper($protocol) . " packets</h2>";
         ?>
         <div class = "content-box">
-            <table>
                 <?php
                     $filter = $_POST['filter'];
                     $packetCount = $_POST['packetCount'];
-                
+                    if($filter == "all")
+                        echo "<table style = 'width: 100%;'>";
+                    else
+                        echo "<table>";
+                    
                     //Command to read packets and store them in a .pcap file
                     $cmd = "sudo tcpdump -i enp0s3 -w output.pcap";
                     //Add packet count
@@ -67,156 +70,143 @@
                     if($filter == "packetLength" || $filter == "all"){
                         echo "<th>Packet length</th>";
                     }
+                    if($filter == "all"){
+                        echo "<th>Info</th>";
+                    }
                     echo "</tr>";
                 
-                    echo "<tr>";
                     //Commands specific for TCP packets
                     if($protocol == "tcp"){
-                        if($filter == "timestamp" || $filter == "all"){
-                            echo "<td>";
-                            echo "<pre>";
-                            echo shell_exec($cmd . " | cut -d ' ' -f 1");
-                            echo "</td>";
-                            echo "</pre>";
+                        exec($cmd . " | cut -d ' ' -f 1", $timestamp, $returnVal);
+                        exec($cmd . " | cut -d ' ' -f 3 | cut -d '.' -f 1,2,3,4", $sourceIP, $returnVal);
+                        exec($cmd . " | cut -d ' ' -f 3 | cut -d '.' -f 5", $sourcePort, $returnVal);
+                        exec($cmd . " | cut -d ' ' -f 5 | cut -d '.' -f 1,2,3,4", $destinationIP, $returnVal);
+                        exec($cmd . " | cut -d ' ' -f 5 | cut -d '.' -f 5 | cut -d ':' -f 1", $destinationPort, $returnVal);
+                        exec($cmd . " -e | cut -d ' ' -f 2", $sourceMAC, $returnVal);
+                        exec($cmd . " -e | cut -d ' ' -f 4 | cut -d ',' -f 1", $destinationMAC, $returnVal);
+                        exec($cmd . " | awk '{print $(NF)}'", $packetLength, $returnVal);
+                        exec($cmd . " | grep -v 'IP6' | cut -d ' ' -f 6- | rev | cut -d ' ' -f 1,2 --complement | cut -c 1 --complement | rev", $info, $returnVal);
+                        for($i = 0; $i < sizeof($timestamp); $i += 1){
+                            echo "<tr>";
+                            if($filter == "timestamp" || $filter == "all"){
+                                echo "<td>".$timestamp[$i]."\n</td>";
+                            }
+                            if($filter == "sourceIP" || $filter == "all"){
+                                echo "<td>".$sourceIP[$i]."\n</td>";
+                            }
+                            if($filter == "sourcePort" || $filter == "all"){
+                                echo "<td>".$sourcePort[$i]."\n</td>";
+                            }
+                            if($filter == "destinationIP" || $filter == "all"){
+                                echo "<td>".$destinationIP[$i]."\n</td>";
+                            }
+                            if($filter == "destinationPort" || $filter == "all"){
+                                echo "<td>".$destinationPort[$i]."\n</td>";
+                            }
+                            if($filter == "sourceMAC" || $filter == "all"){
+                                echo "<td>".$sourceMAC[$i]."\n</td>";
+                            }
+                            if($filter == "destinationMAC" || $filter == "all"){
+                                echo "<td>".$destinationMAC[$i]."\n</td>";
+                            }
+                            if($filter == "packetLength" || $filter == "all"){
+                                echo "<td>".$packetLength[$i]."\n</td>";
+                            }
+                            if($filter == "all"){
+                                echo "<td>".$info[$i]."\n</td>";
+                            }
+                            echo "</tr>";
                         }
-                        if($filter == "sourceIP" || $filter == "all"){
-                            echo "<td>";
-                            echo "<pre>";
-                            echo shell_exec($cmd . " | cut -d ' ' -f 3 | cut -d '.' -f 1,2,3,4");
-                            echo "</td>";
-                            echo "</pre>";
-                        }
-                        if($filter == "sourcePort" || $filter == "all"){
-                            echo "<td>";
-                            echo "<pre>";
-                            echo shell_exec($cmd . " | cut -d ' ' -f 3 | cut -d '.' -f 5");
-                            echo "</td>";
-                            echo "</pre>";
-                        }
-                        if($filter == "destinationIP" || $filter == "all"){
-                            echo "<td>";
-                            echo "<pre>";
-                            echo shell_exec($cmd . " | cut -d ' ' -f 5 | cut -d '.' -f 1,2,3,4");
-                            echo "</td>";
-                            echo "</pre>";
-                        }
-                        if($filter == "destinationPort" || $filter == "all"){
-                            echo "<td>";
-                            echo "<pre>";
-                            echo shell_exec($cmd . " | cut -d ' ' -f 5 | cut -d '.' -f 5 | cut -d ':' -f 1");
-                            echo "</td>";
-                            echo "</pre>";
-                        }
-                        if($filter == "sourceMAC" || $filter == "all"){
-                            echo "<td>";
-                            echo "<pre>";
-                            echo shell_exec($cmd . " -e | cut -d ' ' -f 2");
-                            echo "</td>";
-                            echo "</pre>";
-                        }
-                        if($filter == "destinationMAC" || $filter == "all"){
-                            echo "<td>";
-                            echo "<pre>";
-                            echo shell_exec($cmd . " -e | cut -d ' ' -f 4 | cut -d ',' -f 1");
-                            echo "</td>";
-                            echo "</pre>";
-                        }
-                        if($filter == "packetLength" || $filter == "all"){
-                            echo "<td>";
-                            echo "<pre>";
-                            echo shell_exec($cmd . " | awk '{print $(NF)}'");
-                            echo "</td>";
-                            echo "</pre>";
-                        }
-//                        if($filter == "all"){
-//                            echo "<td style = 'overflow-x = scroll;'>";
-//                            echo "<pre>";
-//                            echo shell_exec($cmd . " | grep -v 'IP6' | cut -d ' ' -f 6- | rev | cut -d ' ' -f 1,2 --complement | cut -c 1 --complement | rev");
-//                            echo "</td>";
-//                            echo "</pre>";
-//                        }
-                        //exec($cmd . " | grep -v 'IP6' | cut -d ' ' -f 6- | rev | cut -d ' ' -f 1,2 --complement | cut -c 1 --complement | rev", $outp, $ret);
-                        //echo sizeof($outp);
-                        //echo $outp[1];
-                        
                     }
                     //Commands specific for UDP packets
                     else if($protocol == "udp"){
-                        if($filter == "timestamp" || $filter == "all"){
-                            echo "<td>";
-                            echo "<pre>";
-                            echo shell_exec($cmd . " | grep 'UDP' |grep -v 'NBT' | cut -d ' ' -f 1");
-                            echo "</td>";
-                            echo "</pre>";
-                        }
-                        if($filter == "sourceIP" || $filter == "all"){
-                            echo "<td>";
-                            echo "<pre>";
-                            echo shell_exec($cmd . " | grep 'UDP' | grep -v 'NBT' | cut -d ' ' -f 3 | cut -d '.' -f 1,2,3,4");
-                            echo "</td>";
-                            echo "</pre>";
-                        }
-                        if($filter == "sourcePort" || $filter == "all"){
-                            echo "<td>";
-                            echo "<pre>";
-                            echo shell_exec($cmd . " | grep 'UDP' | grep -v 'NBT' | cut -d ' ' -f 3 | cut -d '.' -f 5");
-                            echo "</td>";
-                            echo "</pre>";
-                        }
-                        if($filter == "destinationIP" || $filter == "all"){
-                            echo "<td>";
-                            echo "<pre>";
-                            echo shell_exec($cmd . " | grep 'UDP' | grep -v 'NBT' | cut -d ' ' -f 5 | cut -d '.' -f 1,2,3,4");
-                            echo "</td>";
-                            echo "</pre>";
-                        }
-                        if($filter == "destinationPort" || $filter == "all"){
-                            echo "<td>";
-                            echo "<pre>";
-                            echo shell_exec($cmd . " | grep 'UDP' | grep -v 'NBT' | cut -d ' ' -f 5 | cut -d '.' -f 5 | cut -d ':' -f 1");
-                            echo "</td>";
-                            echo "</pre>";
-                        }
-                        if($filter == "sourceMAC" || $filter == "all"){
-                            echo "<td>";
-                            echo "<pre>";
-                            echo shell_exec($cmd . " -e | grep 'UDP' | grep -v 'NBT' | cut -d ' ' -f 2");
-                            echo "</td>";
-                            echo "</pre>";
-                        }
-                        if($filter == "destinationMAC" || $filter == "all"){
-                            echo "<td>";
-                            echo "<pre>";
-                            echo shell_exec($cmd . " -e | grep 'UDP' | grep -v 'NBT' | cut -d ' ' -f 4 | cut -d ',' -f 1");
-                            echo "</td>";
-                            echo "</pre>";
-                        }
-                        if($filter == "packetLength" || $filter == "all"){
-                            echo "<td>";
-                            echo "<pre>";
-                            echo shell_exec($cmd . " | grep 'UDP' | grep -v 'NBT' | awk '{print $(NF)}'");
-                            echo "</td>";
-                            echo "</pre>";
+                        exec($cmd . " | grep 'UDP' |grep -v 'NBT' | cut -d ' ' -f 1", $timestamp, $returnVal);
+                        exec($cmd . " | grep 'UDP' | grep -v 'NBT' | cut -d ' ' -f 3 | cut -d '.' -f 1,2,3,4", $sourceIP, $returnVal);
+                        exec($cmd . " | grep 'UDP' | grep -v 'NBT' | cut -d ' ' -f 3 | cut -d '.' -f 5", $sourcePort, $returnVal);
+                        exec($cmd . " | grep 'UDP' | grep -v 'NBT' | cut -d ' ' -f 5 | cut -d '.' -f 1,2,3,4", $destinationIP, $returnVal);
+                        exec($cmd . " | grep 'UDP' | grep -v 'NBT' | cut -d ' ' -f 5 | cut -d '.' -f 5 | cut -d ':' -f 1", $destinationPort, $returnVal);
+                        exec($cmd . " -e | grep 'UDP' | grep -v 'NBT' | cut -d ' ' -f 2", $sourceMAC, $returnVal);
+                        exec($cmd . " -e | grep 'UDP' | grep -v 'NBT' | cut -d ' ' -f 4 | cut -d ',' -f 1", $destinationMAC, $returnVal);
+                        exec($cmd . " | grep 'UDP' | grep -v 'NBT' | awk '{print $(NF)}'", $packetLength, $returnVal);
+                        exec($cmd . " | grep 'UDP' | grep -v 'NBT' | cut -d ' ' -f 6- | rev | cut -d ' ' -f 1,2 --complement | cut -c 1 --complement | rev", $info, $returnVal);
+                        for($i = 0; $i < sizeof($timestamp); $i += 1){
+                            echo "<tr>";
+                            if($filter == "timestamp" || $filter == "all"){
+                                echo "<td>".$timestamp[$i]."\n</td>";
+                            }
+                            if($filter == "sourceIP" || $filter == "all"){
+                                echo "<td>".$sourceIP[$i]."\n</td>";
+                            }
+                            if($filter == "sourcePort" || $filter == "all"){
+                                echo "<td>".$sourcePort[$i]."\n</td>";
+                            }
+                            if($filter == "destinationIP" || $filter == "all"){
+                                echo "<td>".$destinationIP[$i]."\n</td>";
+                            }
+                            if($filter == "destinationPort" || $filter == "all"){
+                                echo "<td>".$destinationPort[$i]."\n</td>";
+                            }
+                            if($filter == "sourceMAC" || $filter == "all"){
+                                echo "<td>".$sourceMAC[$i]."\n</td>";
+                            }
+                            if($filter == "destinationMAC" || $filter == "all"){
+                                echo "<td>".$destinationMAC[$i]."\n</td>";
+                            }
+                            if($filter == "packetLength" || $filter == "all"){
+                                echo "<td>".$packetLength[$i]."\n</td>";
+                            }
+                            if($filter == "all"){
+                                echo "<td>".$info[$i]."\n</td>";
+                            }
+                            echo "</tr>";
                         }
                     }
                     //Commands specific for ARP packets
-                    else
-                    echo "<tr>";
-
-                    /*
-                    //To read live output from shell
-                    $proc = popen($cmd, 'r');
-                    echo "<pre>";
-                    while(!feof($proc)){
-                        echo fread($proc, 32);//Flush output after every 32 bytes
-                        @ flush();
+                    else{
+                        exec($cmd . " | cut -d ' ' -f 1", $timestamp, $returnVal);
+                        //exec($cmd . " | cut -d ' ' -f 3 | cut -d '.' -f 1,2,3,4", $sourceIP, $returnVal);
+                        //exec($cmd . " | cut -d ' ' -f 3 | cut -d '.' -f 5", $sourcePort, $returnVal);
+                        //exec($cmd . " | cut -d ' ' -f 5 | cut -d '.' -f 1,2,3,4", $destinationIP, $returnVal);
+                        //exec($cmd . " | cut -d ' ' -f 5 | cut -d '.' -f 5 | cut -d ':' -f 1", $destinationPort, $returnVal);
+                        exec($cmd . " -e | cut -d ' ' -f 2", $sourceMAC, $returnVal);
+                        exec($cmd . " -e | cut -d ' ' -f 4 | cut -d ',' -f 1", $destinationMAC, $returnVal);
+                        exec($cmd . " | awk '{print $(NF)}'", $packetLength, $returnVal);
+                        exec($cmd . " | cut -d ',' -f 2 | cut -c 1 --complement", $info, $returnVal);
+                        for($i = 0; $i < sizeof($timestamp); $i += 1){
+                            echo "<tr>";
+                            if($filter == "timestamp" || $filter == "all"){
+                                echo "<td>".$timestamp[$i]."\n</td>";
+                            }
+                            if($filter == "sourceIP" || $filter == "all"){
+                                echo "<td>"."NA"."\n</td>";
+                            }
+                            if($filter == "sourcePort" || $filter == "all"){
+                                echo "<td>"."NA"."\n</td>";
+                            }
+                            if($filter == "destinationIP" || $filter == "all"){
+                                echo "<td>"."NA"."\n</td>";
+                            }
+                            if($filter == "destinationPort" || $filter == "all"){
+                                echo "<td>"."NA"."\n</td>";
+                            }
+                            if($filter == "sourceMAC" || $filter == "all"){
+                                echo "<td>".$sourceMAC[$i]."\n</td>";
+                            }
+                            if($filter == "destinationMAC" || $filter == "all"){
+                                echo "<td>".$destinationMAC[$i]."\n</td>";
+                            }
+                            if($filter == "packetLength" || $filter == "all"){
+                                echo "<td>".$packetLength[$i]."\n</td>";
+                            }
+                            if($filter == "all"){
+                                echo "<td>".$info[$i]."\n</td>";
+                            }
+                            echo "</tr>";
+                        }
                     }
-                    echo "</pre>";
-                    */
-                
+                    echo "</table>";
                 ?>
-            </table>
-            <p><u>Note</u>:- Some packets might be missing due to insufficient data</p>
+            <p><u>Note</u>:- Unsupported packets have been dropped</p>
         </div>
     </body>
     <footer>
